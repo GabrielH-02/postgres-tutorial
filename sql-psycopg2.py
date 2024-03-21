a2df967f21cd58355ad334a8ea7086a0d2ad4d5d
Gabriel Holmes <gabrielholmes2002@gmail.com>
2024-03-21 16:25:28 +0000

    add again

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -23,7 +23,7 @@ cursor = connection.cursor()
 # cursor.execute('SELECT * FROM "album" WHERE "artist_id" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
